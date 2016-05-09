a994c570e69bd9351d2e59d08ec182d050be9893
XoticRealmsOwnerandDev <XoticRealmshelp@gmail.com>
2016-05-09 13:00:06 -0500

    Create main.php

---
+++ src/XoticRealmsOwnerandDev/main.php
@@ -0,0 +1,19 @@
+this will teach you a bunch of differnt plugin php so you can learn how to code your own plugins/php for plugins/plugin.yml:confi
+
+
+Blubberboy333's plugins
+Cratekeys (needs fixes:and testing)
+VoteRanks (not done)
+Custom-enchnats (not done)
+SkyBlock (working)
+Simple-kits (needs fixes:and testing:and editing)             
+perworldban (working)
+ArmorHeal (working)
+
+
+
+XoticRealmsOwnerAndDev's plugins
+Cratekeys (helped with config and plugin.yml)
+custom-enchants (helping blubber boy with config and plugin.yml)
+Simeple-Kits (needs fixes:and testing:and editing)
+
